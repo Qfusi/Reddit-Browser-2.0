6d262e9c80a660efc7a82e749b55222acc0469c5
Qfusi <Jonathan.Isaksson93@gmail.com>
2021-12-19 17:55:09 +0100

    Middleware requires secureCookie in production

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,18 +1,21 @@
 import { getToken } from "next-auth/jwt"
-import { getSession } from "next-auth/react";
 import { NextResponse } from "next/server"
 
 export async function middleware(req) {
     // Token will exist if user is logged in
-    const token = await getToken({ req, secret: process.env.JWT_SECRET });
-    const session = await getSession({ req })
-
+    const token = await getToken({
+        req,
+        secret: process.env.JWT_SECRET,
+        secureCookie: process.env.NEXTAUTH_URL?.startsWith("https://") ??
+        !!process.env.VERCEL_URL,
+    });
+    
     const { pathname } = req.nextUrl;
-    if (pathname.includes("/api/auth") || session) {
+    if (pathname.includes("/api/auth") || token) {
         return NextResponse.next();
     }
     
-    if (!session && pathname !== "/login") {
+    if (!token && pathname !== "/login") {
         return NextResponse.redirect("/login");
     }
 }
