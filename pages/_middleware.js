ac134b9bd8454c69ff878031d9905b4162f16e19
Qfusi <Jonathan.Isaksson93@gmail.com>
2021-12-19 17:42:01 +0100

    test

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,13 +4,14 @@ import { NextResponse } from "next/server"
 export async function middleware(req) {
     // Token will exist if user is logged in
     const token = await getToken({ req, secret: process.env.JWT_SECRET });
-    
+    const session = await getSession({ req })
+
     const { pathname } = req.nextUrl;
-    if (pathname.includes("/api/auth") || token) {
+    if (pathname.includes("/api/auth") || session) {
         return NextResponse.next();
     }
     
-    if (!token && pathname !== "/login") {
+    if (!session && pathname !== "/login") {
         return NextResponse.redirect("/login");
     }
 }
